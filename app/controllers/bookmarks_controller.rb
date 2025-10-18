a8eeabd19180253643b6b6e617ad8389868b9bbc
middwinantony <middwin@gmail.com>
2025-10-18 13:23:31 -0400

    one more test case pending

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,37 @@
+class BookmarksController < ApplicationController
+  before_action :set_list, only: [:index, :new, :create]
+
+  def index
+    @bookmarks = @list.bookmarks.inlcudes(:list)
+  end
+
+  def new
+    @bookmark = @list.bookmarks.new
+  end
+
+  def create
+    @bookmark = @list.bookmarks.new(bookmark_params)
+    if @bookmark.save
+      redirect_to @list
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to @bookmark.list
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+end
